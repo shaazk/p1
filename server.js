49c4ffc7a1bd02934bd2b00ef898277b29f459bf
frankhn <harfrank2@gmail.com>
2019-09-08 19:10:02 +0200

    work for Sharon p2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import app from './app'
 import 'dotenv/config'
+import app from './app'
 
 app.listen(process.env.port || 5000, ()=> {
-    console.log("server running:")
+    console.log("server running:", process.env.port)
 })
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,13 @@
 class User {
-    static getUser(){}
+    static getUser(req, res){
+        const { name, age, country } = req.body;
+        return res.status(200).json({
+            status: 200,
+            name,
+            age,
+            country
+        })
+    }
 }
 
 export default User;
